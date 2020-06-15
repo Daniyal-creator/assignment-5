13722f34cd16177eeed63a5e5f8abece4be8c19e
Daniyal-creator <58301512+Daniyal-creator@users.noreply.github.com>
2020-06-15 19:36:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,364 @@
+                           //Chapter1//
+//Part:1//
+// alert("Hello User Welcome To our Website!!!")
+
+//Part:2//
+// alert("Error! Please enter a valid password ")
+
+//Part:3//
+// alert("Welcome to Js land..\nHappy Coding!")
+
+
+// alert("Happy Coding!\n " + "<input type=checkbox>")
+//Part:4//
+// alert("Welcome to Js Land..")
+// alert("Happy Coding")
+
+
+//Part:5//
+// console.log(alert("Hello.I can run JS through my web browser`s console"))
+
+
+
+                // Chapter:2//
+//task:1//
+//var username
+
+//task:2//
+//var myname = "daniyal aslam"
+
+//task:3//
+//var message = "hello World"
+//alert(message)
+
+//task:4//
+// var name = "Daniyal"
+// var age = "19 years old"
+// var coarse = "Certified Mobile Application Development"
+// alert(name)
+// alert(age)
+// alert(coarse)
+
+//task:5//
+// var a = "PIZZA\nPIIZ\nPIZ\nPI\nP"
+// alert(a)
+
+// //task:6//
+// var email = "example@example.com"
+// alert("My email address is "+email)
+
+//task:7//
+// var book = "A smarter way to learn JavaScript"
+// alert("I am trying to learn the Book " + book)
+
+//task:8//
+// document.write("<h1>Yah! I can write HTML content through Javascript<h1/>")
+
+//task:9//
+// var variable = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬" 
+// alert(variable)
+
+
+
+                //chapter 3//
+//task:1//
+// var age = 19
+// alert(age)
+
+//task:2//
+// var info = "You have visited the site 4 times" 
+// alert(info )
+
+//task:3//
+// var birth_Year = 2000
+// document.write("My birth year is "+birth_Year+"<br>Datatype of my declared variable is "+ typeof(birth_Year))
+
+//task:4//
+// var visitor_name = prompt("Enter your name");
+// var Product_title = prompt("Enter your Product name");
+// var quantity = prompt("Enter your Product Quantity");
+// document.write(  visitor_name + " ordered " + quantity + Product_title +"(s) on DANY Clothing Store")
+
+
+
+
+                            //chapter:5//
+//task:1//
+// var a = prompt("Enter first number");
+// var b = prompt("Enter second number");
+// var c = +a + +b
+// document.write("Sum of "+a +" and " + b + " is " + c)
+
+//task:2//
+// var a = prompt("Enter first number");
+// var b = prompt("Enter second number");
+// var c = +a - +b
+// document.write("Subtraction of "+a +" and " + b + " is " + c +"<br>")
+
+
+// var a = prompt("Enter first number");
+// var b = prompt("Enter second number");
+// var c = +a * +b
+// document.write("Multiplication of "+a +" and " + b + " is " + c +"<br>")
+
+
+// var a = prompt("Enter first number");
+// var b = prompt("Enter second number");
+// var c = +a / +b
+// document.write("Division of "+a +" and " + b + " is " + c + "<br>")
+
+
+// var a = prompt("Enter first number");
+// var b = prompt("Enter second number");
+// var c = +a % +b
+// document.write("Modulus of "+a +" and " + b + " is " + c +"<br>")
+
+//task:3//
+// var a  = "Value after declaration is:??"
+// var b = 5
+// var c  = Number(6) + Number(7)
+// document.write(a+"<br>")
+// document.write("Intial value:"+b+"<br>")
+// document.write("Incrementing value:"+ "<br>")
+// document.write("Value after increment is: "+ ++b +"<br>")
+// document.write("Adding 7 in value"+ "<br>")
+// document.write("Value after addition is: "+ c +"<br>")
+// document.write("Decrement the value" + "<br>")
+// document.write("Value after decrement is: "+ --c +"<br>")
+// document.write("dividing value by 3<br>")
+// document.write("The remainder is: " + ((c)%3))
+
+//task:4//
+// var a = prompt("Enter number of tickets")
+// var b = prompt("Enter the price of a ticket")
+// document.write("Total cost to buy "+a+" tickets to a movie is "+a*b)
+
+//task:5//
+// var a  = prompt("Enter the table number: ")
+// var b  = prompt("Enter number of multiplication of table: ")
+// for (var c = 1 ;c <= b ; c++){
+//     document.write(a + " x " + c +  " = " +  a*c +"<br>") 
+// }
+
+//task:6//
+// var C  = prompt("Enter the tempertaure for celius: ")
+// var F = ((C * (9/5)) + 32)
+// document.write(C + " `C is " + F +"`F <br>")
+
+// var f = prompt("Enter the temperature for ferhenheit:")
+// var c = ((f-32)*5/9)
+// document.write(f + " `F is " + c + "`C <br>")
+
+//task:7//
+// var Price_of_item1 = prompt("Price_of_item1")
+// var Price_of_item_2 = prompt("Price_of_item_2") 
+// var Ordered_quantity_of_item_1 = prompt("Ordered_quantity_of_item_1") 
+// var Ordered_Quantity_of_item_2 = prompt("Ordered_Quantity_of_item_2")
+// var Shipping_charges = prompt("Shipping_charges")
+// var total = +(Price_of_item1*Ordered_quantity_of_item_1) + +(Price_of_item_2*Ordered_Quantity_of_item_2) + +Shipping_charges
+// // var total_cost_of_order = prompt("total_cost_of_order")
+
+// document.write("Price of item 1 is: "+ Price_of_item1 + "<br>" )
+// document.write("Quantity of item 1 is:" +Ordered_quantity_of_item_1 +"<br>")
+// document.write("Price of item 2 is: "+ Price_of_item_2 + "<br>" )
+// document.write("Quantity of item 2 is:" +Ordered_Quantity_of_item_2 +"<br>")
+// // document.write("Total Cost: " +(+(Price_of_item1*Ordered_quantity_of_item_1)))
+// // document.write("Total Cost: " +(+(Price_of_item1*Ordered_quantity_of_item_1)))
+// document.write("Total Cost:" + total ) 
+
+//task:8//
+// var a = prompt("Number obtained ")
+// var b = prompt("Total marks")
+// var p = (a/b)*100
+// document.write("Number obtained "+ a +"<br>")
+// document.write("Total marks"+ b +"<br>")
+// document.write("Percentage ="+p)
+
+                            //chapter:6-9//
+
+//task:1//                            
+
+// var a  = 10
+// document.write("Result <br>")
+// document.write("The value of a is: "+a + "<br>")
+// document.write("<hr>")
+
+
+// document.write("The value of ++a is: "+ ++a +"<br>")
+// document.write("The value of a is: "+ a +"<br>")
+
+// document.write("<br>")
+// document.write("The value of a++ is: " +  a++ + "<br>")
+// document.write("The value of a is: "+a + "<br>")
+// document.write("<br>")
+// document.write("The value of --a is: "+ --a + "<br>")
+// document.write("The value of a is: "+a + "<br>")
+// document.write("<br>")
+// document.write("The value of a-- is: "+ a-- + "<br>")
+// document.write("The value of a is: "+a + "<br>")
+
+//task:2//
+// var a = 2
+// var b = 1
+// document.write("<br>")
+// document.write("To give answer of -a --b ++b + b-- <br><br>")
+// document.write("At --a; The initial value of a is 2, here it is pre-decrement of a so therefore a becomes 1  <br>")
+// document.write("At --a - --b: since a is now 1 and is subtracting with  <br>")
+// // document.write(--a + "<br>");
+// // document.write( --a - --b+ "<br>");
+// // document.write( --a - --b + ++b+ "<br>" );
+// document.write( --a - --b + ++b + b-- + "<br>"); 
+// // document.write("<br>" + (-a - --b + ++b + b--+ "<br>"))
+// document.write("<br>"+a)
+// document.write("<br>" +b)
+
+
+
+
+
+                //chapter:9-11//
+// task:1//
+// a = prompt("Enter the city name: ")
+// if (a == "Karachi" ){
+//     alert("Welcome to city of lights")
+// }
+
+//tsk:2//
+// a = prompt("Enter your gender: ")
+// if (a == "male"){
+//     alert("Good morning sir")
+// }
+// else if (a == "female"){
+//     alert("Good morning Ma`am")
+// }
+
+
+
+
+
+
+
+//chapter:14-16//
+
+//task:1-7//
+// var students = []
+// var str_array = ["daniyal","aslam"]
+// var num_array = [1,2,3,4,5]
+// var bool_array = [true,false]
+// var mix_array = ["daniyal",1,true]
+// var available_qualification = ["SSC","HCS","BCS","BS","BCOM","MS","M.PHIL","PhD"]
+// document.write("Qualifications <br>")
+// for (var i = 0 ;i <available_qualification.length;i++){
+//     document.write((i+1)+") " + available_qualification[i] + "<br>")
+
+// }
+
+//task:8//
+// var name = new Array("Michael", "John", "Tony")
+// var cars = ["Saab", "Volvo", "BMW"];
+// var marks = [320,230,480];
+// console.log(typeof(cars[0]))
+// console.log(typeof(name))
+// console.log(typeof(marks))
+// document.write("Score of" + name.slice(0,7) + " is " + marks[0]+" Percentage: " +((marks[0]/500)*100) + "<br>")
+// document.write("Score of" + name.slice(7,12) + " is " + marks[1]+" Percentage: " + ((marks[1]/500)*100) + "<br>")
+// document.write("Score of" + name[2] + " is " + marks[2]+" Percentage: " + ((marks[2]/500)*100) + "<br>")
+
+
+
+
+
+
+                         //chapter:17-20//
+//task:1//
+// var a  = [[],[],[]]
+// document.write(a)
+
+//task:2//
+// var a = [[0,1,2,3],[1,0,1,2],[2,1,0,1]]
+// document.write(a)
+
+//task:3//
+// for (var c = 1 ;c <= 10 ; c++){
+//         document.write(c+"<br>") 
+// }
+
+//task:4//
+// var a  = prompt("Enter the table number: ")
+// var b  = prompt("Enter number of multiplication of table: ")
+// for (var c = 1 ;c <= b ; c++){
+//     document.write(a + " x " + c +  " = " +  a*c +"<br>") 
+// }
+
+//task:5//
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"] 
+// for (var i = 0; i <fruits.length;i++){
+//     document.write(fruits.slice(i,i+1)+"<br>")
+// }
+
+// for (var i = 0; i <fruits.length;i++){
+//     document.write("Element at index " + i + " is "+fruits.slice(i,i+1)+"<br>")
+// }
+
+//task:6//
+// document.write("<h1>Counting<h1/><br>");
+// for (var i = 1; i<=15;i++){
+//     document.write(i + " ");
+
+// }
+// document.write("<h1>Reverse Counting<h1/><br>");
+// for (var i = 15; i>=1;i--){
+//     document.write(i + " ")
+
+// }
+
+// document.write("<h1>Even Counting<h1/><br>");
+// for (var i = 0; i<=20;i++ ){
+//     if(i%2==0){
+//     document.write(i + " ")
+//     }
+// }
+// document.write("<h1>Odd Counting<h1/><br>");
+// for (var i = 0; i<=20;i++ ){
+//     if(i%2 != 0){
+//     document.write(i + " ")
+//     }
+// }
+
+// document.write("<h1>Even Counting<h1/><br>");
+// for (var i = 2; i<=20;i++ ){
+//     if(i%2==0){
+//     document.write(i +"k" + " ")
+//     }
+// }
+
+//task:7//
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"] 
+// a = prompt("welcome")
+// b = a.toLowerCase()
+// console.log(b)
+// for (var i = 0; i < a.length; i++) {
+//     if (A[i] ===b ) {
+//         document.write(b+" is available at index "+ i + " in our bakery");
+//     }
+// }
+
+//task:8//
+// var array = [240, 53, 78,999, 919, 12]
+// var largest= 0;
+
+// for (var i=0; i<=largest;i++){
+//     if (array[i]>largest) {
+//         var largest=array[i];
+//     }
+// }
+// document.write(largest)
+
+
+
+//task:10//
+// for (var i = 1 ; i<=100 ; i++){
+//     if( i%5 == 0 ){
+//         document.write(i+"<br>")
+//     }
+// }
